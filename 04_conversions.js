47486a611aa09977cf8105bd48e9bb2221f81cea
Phani <phanivankayala879.com>
2025-08-17 23:41:37 +0530

    Day3

---
+++ 04_conversions.js
@@ -0,0 +1,44 @@
+//operations
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+console.log(2+2)
+
+
+
+let str1 = "hello"
+let str2 = "phani"
+let str3 = "good morning"
+
+let str4 = str1+str2+str3
+console.log(str4);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+
+
+console.log("hello")
+
+
+console.log(1+2+"2");
+console.log( (3 + 4) * 5 % 3);
+
+
+//Tricky 
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2 , num3
+ num1=num2=num3 = 2+2
+ let gameCounter = 100
+ ++gameCounter;
+ console.log(gameCounter)
+
+
+
---
+++ 06_datatypesIMP.js
@@ -0,0 +1,76 @@
+/*Data types:
+Primitive DataTypes: There are 7 Types: Call by value
+1.String
+2.Number
+3.Boolean
+4.null
+5.Undefined
+6.Symbol -- Its used to make unqiue
+7.Big INT
+
+Reference (Non Primitive)
+1.Array
+2.Objects
+3.Functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+JavaScript is dynamic type
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
