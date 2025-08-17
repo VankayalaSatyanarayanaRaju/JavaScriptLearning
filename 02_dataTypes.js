0901d7026e9635433580421f171fcc498d312f95
Phani <phanivankayala879.com>
2025-08-17 20:01:07 +0530

    Day2

---
+++ 02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict";    // treat all JS code as newer version
+
+
+//alert (3 + 3)// we are using node.js , not browser
+
+
+console.log(3 + 3)
+console.log("Phani")
+
+
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+
+// PRIMITVE DATA TYPES
+//number ==> 2 to power 53
+//big int 
+// string => ""
+// boolean => true/false
+// null => standalone value
+//undefined => 
+// symbol => unique
+
+
+
+// object
+console.log(typeof "phani");
+console.log(typeof 18)
+console.log(typeof null)   // object
+console.log(typeof undefined)   //undefined
+
+
+
---
+++ 03_conversionoperation.js
@@ -0,0 +1,31 @@
+let scorce = 33
+
+
+// try with "33" => 33
+// "33abc" => NaN
+// true => 1 false =>0
+
+
+
+console.log(typeof scorce);
+console.log(typeof(scorce));
+
+
+let valueInNumber = Number(scorce)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true 0=> false
+//"" => false
+// "phani" => true
+
+let someNumber = 33
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
