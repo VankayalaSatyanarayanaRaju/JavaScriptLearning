30631cf0054551d4811cd930889a40f8508ec4a6
Phani <phanivankayala879.com>
2025-08-18 23:52:26 +0530

    Day4

---
+++ 07_Operations.js
@@ -0,0 +1,26 @@
+// +++++++++++++++++++++++++++++++++++++++++
+
+
+
+// memory are 2 types 
+//stack(Primitive) and heap(Non-primitive) memory
+/*
+stack lo ref var    
+heap loo object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
